ddd9a505c3a44ce6f9a5f42ae5505b894e69bdbf
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-25 16:09:03 +0000

    deploy: d96a374ade6c54e4cdf6f833e60e0ae13b04376a

---
+++ _nuxt/static/1624637342/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprojects\u002Ffullscreen-youtube-player\u002Fplay","\u002Fprojects\u002Fapril-fools-directory","\u002Fblog","\u002Fprojects","\u002Fblog\u002FHello-World"]})

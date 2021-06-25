5f3542d4501cc9f09ec168c7894d3e09076c0ba8
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-25 18:04:30 +0000

    deploy: 1e74a5002bad974faf28bab7e5e0891005e2a285

--- _nuxt/static/1624637342/projects/fullscreen-youtube-player/play/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624637342",layout:"blank",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Ffullscreen-youtube-player\u002Fplay",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8eeecff6"}}}}(null));

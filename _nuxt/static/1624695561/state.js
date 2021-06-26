6fe8470a311cc38dbf244fe3ffccb00954b358be
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-26 08:38:57 +0000

    deploy: fe7a83610c94769aef3a72ca818581d53e7638e5

--- _nuxt/static/1624695561/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624695561",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"43f7d456"}}}}(null,"\u002F"));

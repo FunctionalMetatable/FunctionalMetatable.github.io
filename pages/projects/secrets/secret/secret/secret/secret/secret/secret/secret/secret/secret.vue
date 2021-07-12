99aab758ebcb61ac9688f0bf5849a5b6ce8f4880
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-07-12 19:54:45 +0100

    :)

---
+++ pages/projects/secrets/secret/secret/secret/secret/secret/secret/secret/secret/secret.vue
@@ -0,0 +1,39 @@
+<template>
+  <iframe
+    :src="`https://youtube.com/embed/kbQS-rLmJv4?autoplay=true`"
+    :width="width"
+    :height="height"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+<script>
+export default {
+  layout: "blank",
+  head() {
+    return {
+      title: "Fullscreen YouTube Player",
+    };
+  },
+  data() {
+    return {
+      width: 100,
+      height: 100
+    };
+  },
+  mounted() {
+    let _this = this;
+    function resize() {
+      _this.width = window.innerWidth;
+      _this.height = window.innerHeight;
+    }
+    window.addEventListener("resize", resize);
+    resize();
+  },
+};
+</script>
+<style>
+body {
+  overflow: hidden;
+}
